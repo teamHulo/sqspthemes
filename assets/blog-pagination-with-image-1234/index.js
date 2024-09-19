2354487a8b8d8123650b7a864ec3df80b787cf6c
teamHulo <132452681+teamHulo@users.noreply.github.com>
2024-09-19 11:35:59 +0300

    Update index.js

--- assets/blog-pagination-with-image-1234/index.js
+++ assets/blog-pagination-with-image-1234/index.js
@@ -1,1 +1,87 @@
-!function(e,t){let r;function i(e){let[t,r,i]=e.split(", ")[0].split("."),l=new Date(`${i}-${r}-${t}`);return l.toLocaleDateString("en-US",{month:"short",day:"numeric",year:"numeric"})}fetch(document.location.origin+e+"?format=json-pretty").then(e=>e.json()).then(e=>{r=e.collection.id;let l=function e(t){let r=[];for(let i=0;i<t.items.length;i++){let l,n;l=0===i?t.items.length-1:i-1,n=i===t.items.length-1?0:i+1;let o=new Date(t.items[l].updatedOn).toLocaleString(),s=new Date(t.items[n].updatedOn).toLocaleString(),a=new Date(t.items[i].updatedOn).toLocaleString();r.push({id:t.items[i].id,urlPrev:t.items[l].assetUrl,urlNext:t.items[n].assetUrl,datePrev:o,dateNext:s,dateCurrent:a})}return r}(e);!function e(l){document.querySelector(`.collection-${r}.view-item`).classList.add("blog-img-pagination");let n=document.querySelector(`.collection-${r}.view-item .blog-item-wrapper`),o=document.documentElement;if(n){let s=document.head.querySelector("[property='og:image']").content,a=document.body.getAttribute("id");o.style.setProperty("--project-bg",`url(${s})`),l.forEach((e,r)=>{if(a.includes(e.id)){let l=e.urlPrev,n=e.urlNext,s=i(e.datePrev),c=i(e.dateNext);t&&(o.style.setProperty("--date-prev",`"${s}"`),o.style.setProperty("--date-next",`"${c}"`)),o.style.setProperty("--blog-pagination-prev",`url(${l})`),o.style.setProperty("--blog-pagination-next",`url(${n})`)}})}}(l)})}(values.path,values.isDisplayDate);
+(function(path, isDisplayDate){
+        let collectionId;
+    
+  		fetch(document.location.origin + path +'?format=json-pretty')
+
+            .then(response => response.json())
+            .then(data => { 
+          
+   				collectionId = data.collection.id;
+        		const blogList = buildList(data);
+
+          		setPaginationImg(blogList);                                     
+        })
+  
+  function buildList(data) {
+	const blogList = [];
+    
+  	for (let i = 0; i < data.items.length; i++) {
+      	let prevIndex;
+      	let nextIndex;
+      
+      	
+      if(i === 0) {
+        	prevIndex = data.items.length-1
+        } else {
+        	prevIndex = i - 1
+        }
+      
+       if(i === data.items.length-1) {
+        	nextIndex = 0
+        } else {
+        	nextIndex = i + 1;
+        }
+      
+      const datePrev = (new Date(data.items[prevIndex].updatedOn)).toLocaleString(); 
+      const dateNext = (new Date(data.items[nextIndex].updatedOn)).toLocaleString(); 
+      const dateCurrent = (new Date(data.items[i].updatedOn)).toLocaleString(); 
+           
+    	blogList.push({id: data.items[i].id, urlPrev: data.items[prevIndex].assetUrl, urlNext: data.items[nextIndex].assetUrl, datePrev, dateNext, dateCurrent})
+    }
+    
+    	return blogList;
+  }
+    
+    
+    function dateFormating(dateString) {
+    	 const [day, month, year] = dateString.split(", ")[0].split(".");
+          const date = new Date(`${year}-${month}-${day}`);
+          const options = { month: 'short', day: 'numeric', year: 'numeric' };
+          return date.toLocaleDateString('en-US', options);
+    }
+    
+  
+      function setPaginationImg (list) {
+       
+       document.querySelector(`.collection-${collectionId}.view-item`).classList.add('blog-img-pagination')
+       const blogItem =  document.querySelector(`.collection-${collectionId}.view-item .blog-item-wrapper`);
+       const root = document.documentElement;
+       
+    if (blogItem){
+  	  const blogItemImgSrc = document.head.querySelector("[property='og:image']").content; 
+      const blogId = document.body.getAttribute('id');
+      root.style.setProperty('--project-bg', `url(${blogItemImgSrc})`);
+      
+     
+      list.forEach((el, i) => {
+	      console.log('el', el)
+        if(blogId.includes(el.id)){
+           let urlPrev = el.urlPrev;
+           let urlNext = el.urlNext;
+           
+           let datePrev = dateFormating(el.datePrev);
+           let dateNext = dateFormating(el.dateNext);
+  
+          if(isDisplayDate) {
+          	  root.style.setProperty('--date-prev', `"${datePrev}"`);
+              root.style.setProperty('--date-next', `"${dateNext}"`);
+          }
+          
+          root.style.setProperty('--blog-pagination-prev', `url(${urlPrev})`);
+          root.style.setProperty('--blog-pagination-next', `url(${urlNext})`);
+   
+           }
+      	})
+    	}
+      }   
+  })(values.path, values.isDisplayDate)
